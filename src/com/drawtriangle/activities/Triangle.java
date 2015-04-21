39ec8cbb569939918a48cd97d8055b11546ca33b
saqibkhalil91 <saqibkahlil91@gmail.com>
2015-04-21 13:46:43 +0500

    Formatting

--- src/com/drawtriangle/activities/Triangle.java
+++ src/com/drawtriangle/activities/Triangle.java
@@ -1,6 +1,5 @@
 package com.drawtriangle.activities;
 
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -17,37 +16,35 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class Triangle extends Activity {
-	private double length1,length2,length3,angle1,angle2,angle3;
+	private double length1, length2, length3, angle1, angle2, angle3;
 	int toggle_flag;
 	TriangleView triangle;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getBundleValues();
-	     setContentView(triangle);
-	
-		
-	
-	   
+		setContentView(triangle);
+
 	}
-	private void getBundleValues()
-	{
+
+	private void getBundleValues() {
 		triangle = new TriangleView(this);
 		Bundle getBundle = this.getIntent().getExtras();
-		 length1 = getBundle.getDouble("l1");
-		 triangle.setLength1(length1);
-	     length2 = getBundle.getDouble("l2");
-	     triangle.setLength2(length2);
-	     length3 = getBundle.getDouble("l3");
-	     triangle.setLength3(length3);
-	     angle1 = getBundle.getDouble("a1");
-	     triangle.setAngle1(angle1);
-	     angle2 = getBundle.getDouble("a2");
-	     triangle.setAngle2(angle2);
-	     angle3 = getBundle.getDouble("a3");
-	     triangle.setAngle3(angle3);
-	     toggle_flag=getBundle.getInt("toggle");
-	     triangle.setToggle(toggle_flag);
+		length1 = getBundle.getDouble("l1");
+		triangle.setLength1(length1);
+		length2 = getBundle.getDouble("l2");
+		triangle.setLength2(length2);
+		length3 = getBundle.getDouble("l3");
+		triangle.setLength3(length3);
+		angle1 = getBundle.getDouble("a1");
+		triangle.setAngle1(angle1);
+		angle2 = getBundle.getDouble("a2");
+		triangle.setAngle2(angle2);
+		angle3 = getBundle.getDouble("a3");
+		triangle.setAngle3(angle3);
+		toggle_flag = getBundle.getInt("toggle");
+		triangle.setToggle(toggle_flag);
 	}
 
 	@Override
@@ -68,5 +65,5 @@ public class Triangle extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 }
